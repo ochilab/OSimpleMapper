5dece64454b07f31f562db28cc4961686760417f
yochi <yochi@ochi-lab.org>
2012-10-14 15:28:50 +0900

    Minor Fixes

--- org/ochilab/aws/OSimpleMapper.java
+++ org/ochilab/aws/OSimpleMapper.java
@@ -1,6 +1,5 @@
 package org.ochilab.aws;
 
-
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -27,8 +26,8 @@ import com.amazonaws.services.simpledb.util.SimpleDBUtils;
 /**
  * 
  * @author Youji Ochi, Yuki Takubo
- * http://www.ochi-lab.org/research/project/osimplemapper
- *
+ *         http://www.ochi-lab.org/research/project/osimplemapper
+ * 
  */
 public class OSimpleMapper {
 
@@ -36,101 +35,111 @@ public class OSimpleMapper {
 	private String prefix;
 	private String endPoint;
 
-	public OSimpleMapper(String awsId, String secretKey, String endPoint,String prefix) {// コンストラクター
-		_OSimpleDB(awsId, secretKey, endPoint,prefix);
+	public OSimpleMapper(String awsId, String secretKey, String endPoint,
+			String prefix) {// コンストラクター
+		_OSimpleDB(awsId, secretKey, endPoint, prefix);
 	}
-	public OSimpleMapper(String awsId, String secretKey,String endPoint) {// コンストラクター
+
+	public OSimpleMapper(String awsId, String secretKey, String endPoint) {// コンストラクター
 		this.prefix = "";
 		_OSimpleDB(awsId, secretKey, endPoint, prefix);
 	}
 
-	private void _OSimpleDB(String awsId, String secretKey, String endPoint, String prefix){
-		sdb = new AmazonSimpleDBClient(new BasicAWSCredentials(awsId, secretKey));
-	    sdb.setEndpoint(endPoint);
+	private void _OSimpleDB(String awsId, String secretKey, String endPoint,
+			String prefix) {
+		sdb = new AmazonSimpleDBClient(
+				new BasicAWSCredentials(awsId, secretKey));
+		sdb.setEndpoint(endPoint);
 		this.prefix = prefix;
 	}
-	
-	
-	public void put(Object o) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+
+	public void put(Object o) throws IllegalArgumentException,
+			IllegalAccessException, InvocationTargetException {
 
 		HashMap<String, String> map = this.mapping(o);
-			String itemName = map.get("itemName");
-			map.remove("itemName");
-			PutAttributesRequest request = new PutAttributesRequest();
-			Set<String> keyList = map.keySet();
-			for(String key:keyList){
-		   
-		    ReplaceableAttribute data = new ReplaceableAttribute();
-		    data.withName(key).withValue(map.get(key));
+		String itemName = map.get("itemName");
+		map.remove("itemName");
+		PutAttributesRequest request = new PutAttributesRequest();
+		Set<String> keyList = map.keySet();
+		for (String key : keyList) {
 
-		    request.withDomainName(o.getClass().getSimpleName()).withItemName(itemName).withAttributes(data);
+			ReplaceableAttribute data = new ReplaceableAttribute();
+			data.withName(key).withValue(map.get(key));
 
-		    sdb.putAttributes(request);
-			}
-			
-			System.out.println(o.getClass().getSimpleName());
+			request.withDomainName(o.getClass().getSimpleName())
+					.withItemName(itemName).withAttributes(data);
+
+			sdb.putAttributes(request);
+		}
+
+		System.out.println(o.getClass().getSimpleName());
 
 	}
 
-	public void deleteDomain(String domainName){
-		
-	    DeleteDomainRequest request = new DeleteDomainRequest(domainName);
-	    sdb.deleteDomain(request);
-	    System.out.println("ドメイン名:" + domainName + "を削除しました");
+	public void deleteDomain(String domainName) {
+
+		DeleteDomainRequest request = new DeleteDomainRequest(domainName);
+		sdb.deleteDomain(request);
+		System.out.println("ドメイン名:" + domainName + "を削除しました");
 	}
-	
-	
+
 	public void delete(Class<?> c, String itemName) {
-			sdb.deleteAttributes(new DeleteAttributesRequest(c.getSimpleName(), itemName)
-			.withAttributes(new Attribute().withName("author")));
-		
+		sdb.deleteAttributes(new DeleteAttributesRequest(c.getSimpleName(),
+				itemName).withAttributes(new Attribute().withName("author")));
 
 	}
 
-	public List select(Class<?> c, String query) throws SecurityException, IllegalArgumentException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
+	public List select(Class<?> c, String query) throws SecurityException,
+			IllegalArgumentException, InstantiationException,
+			IllegalAccessException, NoSuchMethodException,
+			InvocationTargetException {
 		List list = new ArrayList();
-		    SelectRequest request = new SelectRequest(query,true);
-		    SelectResult result = sdb.select(request);
-		    for (Item row : result.getItems()) {
-		   	 Map<String, String[]> map = new HashMap<String, String[]>();
-			
-				for (Attribute attr : row.getAttributes()) {
-					String value[] = new String[1];
-					value[0]=attr.getValue();
-					map.put(attr.getName(), value);
-				}
-				Object obj = this.mapToObject(c.newInstance().getClass(), map);
-				list.add(obj);
-		    }
+		SelectRequest request = new SelectRequest(query, true);
+		SelectResult result = sdb.select(request);
+		for (Item row : result.getItems()) {
+			Map<String, String[]> map = new HashMap<String, String[]>();
+
+			for (Attribute attr : row.getAttributes()) {
+				String value[] = new String[1];
+				value[0] = attr.getValue();
+				map.put(attr.getName(), value);
+			}
+			Object obj = this.mapToObject(c.newInstance().getClass(), map);
+			list.add(obj);
+		}
 		return list;
 
 	}
-	public Object get(Class<?> c, String itemName) throws SecurityException, IllegalArgumentException, InstantiationException, IllegalAccessException, NoSuchMethodException, InvocationTargetException {
-		Map result;
-		Object obj=null; ;
 
-		GetAttributesResult getAttrResult = 
-				sdb.getAttributes(
-						new GetAttributesRequest()
-							.withDomainName(c.getSimpleName())
-							.withItemName(itemName)
-								);
-				
-		
-		 Map<String, String[]> map = new HashMap<String, String[]>();
-		
+	public Object get(Class<?> c, String itemName) throws SecurityException,
+			IllegalArgumentException, InstantiationException,
+			IllegalAccessException, NoSuchMethodException,
+			InvocationTargetException {
+		Map result;
+		Object obj = null;
+		;
+
+		GetAttributesResult getAttrResult = sdb
+				.getAttributes(new GetAttributesRequest().withDomainName(
+						c.getSimpleName()).withItemName(itemName));
+
+		Map<String, String[]> map = new HashMap<String, String[]>();
+
 		for (Attribute attr : getAttrResult.getAttributes()) {
-			
+
 			String value[] = new String[1];
-			value[0]=attr.getValue();
+			value[0] = attr.getValue();
 			map.put(attr.getName(), value);
 		}
-		
+
 		obj = this.mapToObject(c, map);
-		
+
 		return obj;
 	}
-	private HashMap<String, String> mapping(Object o) throws IllegalArgumentException, IllegalAccessException, InvocationTargetException {
+
+	private HashMap<String, String> mapping(Object o)
+			throws IllegalArgumentException, IllegalAccessException,
+			InvocationTargetException {
 
 		Object instance = o;
 		Class<?> c = instance.getClass();// そのオブジェクト内の調査準備
@@ -142,10 +151,10 @@ public class OSimpleMapper {
 																								// フィールドの値>のList
 		HashMap<String, String> ffMap = new HashMap<String, String>();// 一人ずつのデータのために毎回HashMapをニュー
 		for (Map.Entry<String, Method> map : fMethodMap.entrySet()) {
-			
-				Method method = map.getValue();
-				ffMap.put(map.getKey(), method.invoke(o).toString());
-			
+
+			Method method = map.getValue();
+			ffMap.put(map.getKey(), method.invoke(o).toString());
+
 		}
 		return ffMap;
 	}
@@ -154,8 +163,13 @@ public class OSimpleMapper {
 	 * 
 	 * @param list
 	 * @return
+	 * @throws InvocationTargetException
+	 * @throws IllegalAccessException
+	 * @throws IllegalArgumentException
 	 */
-	private List<HashMap<String, String>> mapping(List<Object> list) {
+	private List<HashMap<String, String>> mapping(List<Object> list)
+			throws IllegalArgumentException, IllegalAccessException,
+			InvocationTargetException {
 
 		if (list.size() < 1) {// まず初めにlistの要素数が１以上であるかどうかチェック！
 			return null;
@@ -171,13 +185,10 @@ public class OSimpleMapper {
 				HashMap<String, String> ffMap = new HashMap<String, String>();// 一人ずつのデータのために毎回HashMapをニュー
 																				// 無駄な気がするけどこう書かなければ。
 				for (Map.Entry<String, Method> map : fMethodMap.entrySet()) {
-					try {
-						Method method = map.getValue();
-						ffMap.put(map.getKey(), method.invoke(object)
-								.toString());
-					} catch (Exception er) {
-						er.printStackTrace();
-					}
+
+					Method method = map.getValue();
+					ffMap.put(map.getKey(), method.invoke(object).toString());
+
 				}
 				returnMapList.add(ffMap);
 			}
@@ -201,23 +212,25 @@ public class OSimpleMapper {
 		return map;
 	}
 
-	private Object mapToObject(Class<?> c, Map<String, String[]> map) throws InstantiationException, IllegalAccessException, SecurityException, NoSuchMethodException, IllegalArgumentException, InvocationTargetException {
-		Class<?> clazz=null;
-		Object obj=null;
-		 obj=c.newInstance();
-			clazz = obj.getClass();
-			for (String key : map.keySet()) {// このfor文で一つのオブジェクトが完成！
-				String setterMethodName = "set" + key.substring(0, 1).toUpperCase()
-						+ key.substring(1);// mapのキー値（フィールド名）からセッターメソッド名を推測）
-				String[] setterMethodArgument = (String[])map.get(key);// mapの中身から、セッターメソッドの引数を入手
-					Method method = clazz.getMethod(setterMethodName,
-							new Class[] { String.class });// ↑の２値からセッターメソッド作成
-					method.invoke(obj, setterMethodArgument);// セッターメソッド実行
+	private Object mapToObject(Class<?> c, Map<String, String[]> map)
+			throws InstantiationException, IllegalAccessException,
+			SecurityException, NoSuchMethodException, IllegalArgumentException,
+			InvocationTargetException {
+		Class<?> clazz = null;
+		Object obj = null;
+		obj = c.newInstance();
+		clazz = obj.getClass();
+		for (String key : map.keySet()) {// このfor文で一つのオブジェクトが完成！
+			String setterMethodName = "set" + key.substring(0, 1).toUpperCase()
+					+ key.substring(1);// mapのキー値（フィールド名）からセッターメソッド名を推測）
+			String[] setterMethodArgument = (String[]) map.get(key);// mapの中身から、セッターメソッドの引数を入手
+			Method method = clazz.getMethod(setterMethodName,
+					new Class[] { String.class });// ↑の２値からセッターメソッド作成
+			method.invoke(obj, setterMethodArgument);// セッターメソッド実行
 
-			}
-			//System.out.println(clazz.toString());
-			return obj;
+		}
+		// System.out.println(clazz.toString());
+		return obj;
 	}
-
 
 }
