27d416b96d44702590397541236d78c38ba9b979
ochilab <yochi@ochi-lab.org>
2015-12-25 21:07:09 +0900

    Create Sample DataObject

---
+++ org/ochilab/aws/testTable.java
@@ -0,0 +1,26 @@
+package org.ochilab.aws;
+public class testTable {
+
+	private String[] name;
+	private String age;
+	private String itemName;
+	
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public String[] getName() {
+		return name;
+	}
+	public void setName(String[] name) {
+		this.name = name;
+	}
+	public String getAge() {
+		return age;
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}	
+}
